c16814ee5db32afc0102b01fee14f2e38fb788cf
Idris Akintobi Adejare <akintobiidris@gmail.com>
2022-12-12 14:18:34 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+// Hello world server
+
+const app = express();
+
+app.get("/", (_, res) => {
+  res.status(200).json({ message: "Hello world" });
+});
+
+app.listen(5004, () => console.log("Server started on 5004"));
