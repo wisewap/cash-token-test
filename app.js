18ca1275be0a2b444d6c20a907391ef3cd124965
Mzer Michael <43172150+wisewap@users.noreply.github.com>
2022-12-12 14:59:22 +0100

    Update app.js
    updated the code

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ import express from "express";
 
 // Hello world server
 
-const app = express();
+const application = express();
 
 app.get("/", (_, res) => {
-  res.status(200).json({ message: "Hello world" });
+  response.status(200).json({ message: "Hello world" });
 });
 
 app.listen(5004, () => console.log("Server started on 5004"));
